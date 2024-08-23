e4d25d0cfd9703f3ceb28bd23b94c1e5400690b1
Ruturaj Deshmukh <ruturajd9696@gmail.com>
2024-08-23 19:24:51 +0530

    Routes added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import ChatPage from "./Pages/ChatPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chats" element={<ChatPage />} />
+        </Routes>
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
