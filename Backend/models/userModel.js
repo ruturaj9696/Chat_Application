dc944cf843ea64ddfa5d4ab0dc612bf766fac251
Ruturaj Deshmukh <ruturajd9696@gmail.com>
2024-08-24 10:42:41 +0530

    models added

---
+++ Backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    pic: {
+      type: String,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  { timstamps: true }
+);
+
+const User = mongoose.Model("User", userSchema);
+module.exports = User;
